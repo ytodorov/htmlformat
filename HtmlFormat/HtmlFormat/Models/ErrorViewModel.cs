b32765709a082d5cb88cdc8d9e52bd79bee4f513
ytodorov <ytodorov@ytodorov.com>
2020-03-08 16:16:06 +0200

    Initial Commit

---
+++ HtmlFormat/HtmlFormat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HtmlFormat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
