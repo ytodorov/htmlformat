ca6d400bfd8f190117ccac1a20aa78d4dced82db
ytodorov <ytodorov@ytodorov.com>
2020-03-09 23:56:52 +0200

    format amp

--- HtmlFormat/HtmlFormat/Controllers/HomeController.cs
+++ HtmlFormat/HtmlFormat/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace HtmlFormat.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger;  
         }
 
         public IActionResult Index()
--- HtmlFormat/HtmlFormat/Startup.cs
+++ HtmlFormat/HtmlFormat/Startup.cs
@@ -53,7 +53,7 @@ namespace HtmlFormat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {           
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
